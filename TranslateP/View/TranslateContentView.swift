6f0913ac1e82f556915115d48f62c52997218765
windstormeye <877302410@qq.com>
2025-06-16 00:27:18 +0800

    feat: 完善 代码结构

--- TranslateP/View/TranslateContentView.swift
+++ TranslateP/View/TranslateContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import AVFoundation
 import AppKit
 
 struct TranslateContentView: View {
@@ -86,59 +85,6 @@ struct TranslateContentView: View {
         // 2秒后恢复原始图标
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             isCopied = false
-        }
-    }
-}
-
-class SpeechManager: NSObject, ObservableObject, AVSpeechSynthesizerDelegate {
-    @Published var isSpeaking = false
-    private let synthesizer = AVSpeechSynthesizer()
-    private let speechQueue = DispatchQueue(label: "speech.queue", qos: .background)
-    
-    override init() {
-        super.init()
-        synthesizer.delegate = self
-    }
-    
-    func speakText(_ text: String) {
-        guard !text.isEmpty else { return }
-        
-        if synthesizer.isSpeaking {
-            stopSpeaking()
-            return
-        }
-        
-        speechQueue.async {
-            let utterance = AVSpeechUtterance(string: text)
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-            utterance.rate = 0.5
-            utterance.volume = 1.0
-            
-            DispatchQueue.main.async {
-                self.isSpeaking = true
-            }
-            
-            self.synthesizer.speak(utterance)
-        }
-    }
-    
-    func stopSpeaking() {
-        synthesizer.stopSpeaking(at: .immediate)
-        DispatchQueue.main.async {
-            self.isSpeaking = false
-        }
-    }
-    
-    // MARK: - AVSpeechSynthesizerDelegate
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        DispatchQueue.main.async {
-            self.isSpeaking = false
-        }
-    }
-    
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
-        DispatchQueue.main.async {
-            self.isSpeaking = false
         }
     }
 }
